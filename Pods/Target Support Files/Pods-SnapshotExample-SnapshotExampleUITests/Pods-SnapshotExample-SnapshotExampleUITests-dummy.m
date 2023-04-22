e2ffbfdf24c1ffed5725a4f5e77bf4cda5ca58dd
enadolinskaia <enadolinskaia@tradingview.com>
2023-04-23 02:16:51 +0300

    Init commit

---
+++ Pods/Target Support Files/Pods-SnapshotExample-SnapshotExampleUITests/Pods-SnapshotExample-SnapshotExampleUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SnapshotExample_SnapshotExampleUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SnapshotExample_SnapshotExampleUITests
+@end
